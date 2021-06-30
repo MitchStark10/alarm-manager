7aa58fdaa0f5e9083ee9d94f00dff7fdfbf509c7
Mitch Stark <MitchStark10@gmail.com>
2021-06-30 18:15:41 -0400

    configure typescript react app, get build process working

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import path from 'path';
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', ( req, res ) => {
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/api', ( req, res ) => {
     res.send( 'Hello world!' );
 });
 
 app.listen( port, () => {
     console.log(`server started at http://localhost:${ port }`);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
