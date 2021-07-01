378cb0fbc978feccb9634f036b3ca783ec9af49e
Mitch Stark <MitchStark10@gmail.com>
2021-06-30 21:29:42 -0400

    QueryRunner wrapper added

---
+++ services/QueryRunner.ts
@@ -0,0 +1,71 @@
+import mysql, { PoolConnection, Pool, MysqlError } from 'mysql';
+
+class QueryRunner {
+    pool: Pool;
+
+    constructor() {
+        this.pool = mysql.createPool({
+            host: process.env.MLS_PREDICTOR_HOST,
+            user: process.env.MLS_PREDICTOR_USER,
+            password: process.env.MLS_PREDICTOR_PASSWORD,
+            database: process.env.MLS_PREDICTOR_DATABASE
+        });
+    }
+
+    logQueryResponse(queryText: string, rows: any[]) {
+        console.log('For query: ' + queryText);
+        console.log(JSON.stringify(rows) + ' row(s) returned');
+        console.log('\n\n\n');
+    }
+
+    logError(sqlQuery: string, error: MysqlError) {
+        console.error('Error performing: ' + sqlQuery + '\n' + error + '\n\n');
+    }
+
+    async runQuery(sqlQuery: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.pool.getConnection((err: MysqlError, connection: PoolConnection) => {
+                if (err) {
+                    this.logError(sqlQuery, err);
+                    reject(err);
+                    return;
+                }
+
+                if (!connection) {
+                    reject(new Error('Connection to DB was not created'));
+                    return;
+                }
+
+                connection.query(sqlQuery, (queryError, response) => {
+                    if (queryError) {
+                        this.logError(sqlQuery, queryError);
+                        reject(queryError);
+                        return;
+                    }
+
+                    this.logQueryResponse(sqlQuery, response);
+                    resolve(response);
+                });
+
+                connection.release();
+            });
+        });
+    }
+
+    async runQueryWithErrorHandling(sqlQuery: string): Promise<any> {
+        try {
+            return {
+                success: true,
+                result: await this.runQuery(sqlQuery)
+            };
+        } catch (error) {
+            console.error('Error caught during query:' + error);
+            return {
+                success: false,
+                error
+            };
+        }
+    }
+}
+
+module.exports = new QueryRunner();
