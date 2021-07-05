0aae495b76e10397f5286ce5fb7b3521e871351b
Mitch Stark <MitchStark10@gmail.com>
2021-07-05 15:39:06 -0400

    prep for login state, fix build issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+interface UserData {
+  email: string | null,
+  apiKey: string | null
+};
+
+const EMPTY_USER_INFORMATION = {
+  email: null,
+  apiKey: null
+};
+
+const AuthContext = React.createContext<UserData>(EMPTY_USER_INFORMATION);
+
+const LOGIN_STATES = {
+  'LOADING': 'LOADING',
+  'LOGGED_IN': 'LOGGED_IN',
+  'UNAUTHENTICATED': 'UNAUTHENTICATED'
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [user, setUser] = React.useState<UserData>(EMPTY_USER_INFORMATION);
+  const [loginState, setLoginState] = React.useState(LOGIN_STATES.LOADING);
+
+  React.useEffect(() => {
+    const email = localStorage.getItem('userEmail');
+    const apiKey = localStorage.getItem('apiKey');
+
+    setUser({
+      email,
+      apiKey
+    });
+
+    setLoginState(email && apiKey ? LOGIN_STATES.LOGGED_IN : LOGIN_STATES.UNAUTHENTICATED);
+  }, []);
+
+  if (loginState === LOGIN_STATES.LOADING) {
+    return null;
+  }
+
+  return loginState === LOGIN_STATES.LOGGED_IN ? (
+    <AuthContext.Provider value={user}>
+      <div className="app">
+        <p>You are logged in.</p>
+      </div>
+    </AuthContext.Provider>
+  ) : (
+    <p>TODO: Make this a login component</p>
   );
 }
 
