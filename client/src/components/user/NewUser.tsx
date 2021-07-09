14b1568bedab9b81441c88d9178af53dcc6552cc
Mitch Stark <MitchStark10@gmail.com>
2021-07-08 20:54:02 -0400

    ready for form submission, styling

--- client/src/components/user/NewUser.tsx
+++ client/src/components/user/NewUser.tsx
@@ -1,10 +1,16 @@
+import useInputState from "../../hooks/useInputState";
+
 export default function NewUser() {
+	const [email, , setEmailFromInput] = useInputState('');
+	const [password, , setPasswordFromInput] = useInputState('');
+	const [passwordConfirmation, , setConfirmationFromInput] = useInputState('');
+
 	return (
 		<>
 			<p>New User</p>
-			<input type="text" placeholder="Email"></input>
-			<input type="password" placeholder="Password"></input>
-			<input type="password" placeholder="Confirm Password"></input>
+			<input type="text" placeholder="Email" value={email} onChange={setEmailFromInput}></input>
+			<input type="password" placeholder="Password" value={password} onChange={setPasswordFromInput}></input>
+			<input type="password" placeholder="Confirm Password" value={passwordConfirmation} onChange={setConfirmationFromInput}></input>
 			<button type="button">SUBMIT</button>
 		</>
 	)
---
+++ client/src/hooks/useInputState.ts
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function useInputState(initialValue: any) {
+	const [value, setValue] = useState(initialValue);
+
+	const setValueFromInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+		setValue(e.currentTarget.value);
+	};
+
+	return [value, setValue, setValueFromInputChange];
+}
