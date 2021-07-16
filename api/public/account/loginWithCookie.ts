0bc9deb215c08b1d6a53f6fdc70e5b4e009aa3d5
Mitch Stark <MitchStark10@gmail.com>
2021-07-16 17:02:05 -0400

    switch to eslint, work on login implementation

--- api/public/account/loginWithCookie.ts
+++ api/public/account/loginWithCookie.ts
@@ -10,19 +10,22 @@ WHERE SessionCookie = ?
 `;
 
 app.post('', async (req, res) => {
-    const { sessionCookie } = req.body;
+    const {sessionCookie} = req.body;
 
     if (!sessionCookie) {
         return res.status(403).json({
             success: false,
-            message: 'Session cookie was empty'
+            message: 'Session cookie was empty',
         });
     }
 
-    const retrieveUserQuery = mysql.format(RETRIEVE_USER_FROM_COOKIE, [sessionCookie]);
-    const retrieveUserResponse = await QueryRunner.runQueryWithErrorHandling(retrieveUserQuery);
+    const retrieveUserQuery = mysql.format(RETRIEVE_USER_FROM_COOKIE,
+        [sessionCookie]);
+    const retrieveUserResponse =
+        await QueryRunner.runQueryWithErrorHandling(retrieveUserQuery);
 
-    res.status(retrieveUserResponse.success ? 200 : 403).json(retrieveUserResponse);
+    res.status(retrieveUserResponse.success ? 200 : 403)
+        .json(retrieveUserResponse);
 });
 
 export default app;
--- client/src/components/user/Login.tsx.js
+++ client/src/components/user/Login.tsx.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import useInputState from "../../hooks/useInputState";
+
 export default function Login() {
+    const [email, , setEmail] = useInputState('');
+    const [password, , setPassword] = useInputState();
+    const [error, setError] = useState('');
+
+    const submitLogin = () => {
+        fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                //TODO: Set Email
+                //TODO: Tell container that the login state has switched
+            } else {
+                setError('Email or login is incorrect');
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            setError(error.toString());
+        });
+    };
+
     return (
         <div>
             <p>Login</p>
-            <input type="text" placeholder="Email"></input>
-            <input type="password" placeholder="Password"></input>
-            <button type="button">Submit</button>
+            {error && <p className="error-text">{error}</p>}
+            <input type="text" placeholder="Email" value={email} onChange={setEmail}></input>
+            <input type="password" placeholder="Password" value={password} onChange={setPassword}></input>
+            <button type="button" onClick={submitLogin}>Submit</button>
         </div>
     )
 }
