5d847fb88407865d14b62c136755b9b5f2d2fbf6
play.js <git@gituser.com>
2021-07-15 22:35:31 -0400

    create base login html

---
+++ client/src/components/user/Login.tsx.js
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+        <div>
+            <p>Login</p>
+            <input type="text" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+            <button type="button">Submit</button>
+        </div>
+    )
+}
