ea11ff3c993418888d1995f516de65d0bbc30772
MitchStark10 <mitch.stark.github@gmail.com>
2021-07-25 19:03:56 +0000

    minor styling touchups

--- client/src/components/alarms/AlarmCard.tsx
+++ client/src/components/alarms/AlarmCard.tsx
@@ -10,14 +10,12 @@ interface AlarmCardProps {
 export default function AlarmCard({data}: AlarmCardProps) {
     return (
         <Card style={{width: '18rem'}}>
-            <Card.Img variant="top" src="holder.js/100px180" />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{data.AlarmText}</Card.Title>
                 <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card&apos;s content.
+                    {data.AlarmDateTime}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">Delete Alarm</Button>
             </Card.Body>
         </Card>
     );
--- client/src/components/alarms/AlarmList.tsx
+++ client/src/components/alarms/AlarmList.tsx
@@ -46,10 +46,10 @@ export default function AlarmList({email}: AlarmListProps) {
         <>
             {error && <p className="error-text">{error.toString()}</p>}
             {alarmList.length ?
-                (<>
-                    <p>Alarms found</p>
+                (<div className={"d-flex flex-column justify-content-center align-items-center"}>
+                    <h3 className="border-bottom border-dark px-5 mb-3">Alarms found</h3>
                     {alarmList.map((alarmData) => <AlarmCard data={alarmData} key={alarmData.ID} />)}
-                </>) :
+                </div>) :
                 <p>No alarms found</p>
             }
         </>
