0ec806dc6480b97c4e2c9735054e2152e0c88954
Mitch Stark <MitchStark10@gmail.com>
2021-07-25 13:19:00 -0400

    fix eslint in client directory

--- client/src/components/alarms/AlarmList.tsx
+++ client/src/components/alarms/AlarmList.tsx
@@ -1,58 +1,57 @@
-import { useEffect, useState } from "react";
-import AlarmCard from "./AlarmCard";
-import { AlarmData } from './AlarmTypes';
+import React, {useEffect, useState} from 'react';
+import AlarmCard from './AlarmCard';
+import {AlarmData} from './AlarmTypes';
 
 interface AlarmListProps {
-	email: string
+    email: string
 }
 
-export default function AlarmList({ email }: AlarmListProps) {
-	const [alarmList, setAlarmList] = useState<Array<AlarmData>>([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [error, setError] = useState('');
+export default function AlarmList({email}: AlarmListProps) {
+    const [alarmList, setAlarmList] = useState<Array<AlarmData>>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState('');
 
-	useEffect(() => {
-		const fetchData = {
-			email
-		};
-		fetch('/api/authenticated/alarm/retrieveAlarms', {
-			method: 'POST',
-			body: JSON.stringify(fetchData),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		.then(data => data.json())
-		.then(response => {
-			if (response.success) {
-				setAlarmList(response.result)
-			} else {
-				console.error(response);
-				setError(response.message || response);
-			}
-		})
-		.catch(error => {
-			console.error(error);
-			setError(error.toString());
-		})
-		.finally(() => setIsLoading(false));
-	}, [email]);
+    useEffect(() => {
+        const fetchData = {
+            email,
+        };
+        fetch('/api/authenticated/alarm/retrieveAlarms', {
+            method: 'POST',
+            body: JSON.stringify(fetchData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((data) => data.json())
+            .then((response) => {
+                if (response.success) {
+                    setAlarmList(response.result);
+                } else {
+                    console.error(response);
+                    setError(response.message || response);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                setError(error.toString());
+            })
+            .finally(() => setIsLoading(false));
+    }, [email]);
 
-	if (isLoading) {
-		return <p>Loading...</p>;
-	}
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
 
-	return (
-		<>
-		{error && <p className="error-text">{error.toString()}</p>}
-		{alarmList.length ?
-			(<>
-				<p>Alarms found</p>
-				{alarmList.map((alarmData) => <AlarmCard data={alarmData} key={alarmData.ID} />)}
-			</>)
-			:
-			<p>No alarms found</p>
-		}
-		</>
-	);
+    return (
+        <>
+            {error && <p className="error-text">{error.toString()}</p>}
+            {alarmList.length ?
+                (<>
+                    <p>Alarms found</p>
+                    {alarmList.map((alarmData) => <AlarmCard data={alarmData} key={alarmData.ID} />)}
+                </>) :
+                <p>No alarms found</p>
+            }
+        </>
+    );
 };
