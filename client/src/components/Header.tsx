3170feda2c5c4e29f3194d6df2d2ccd594600fa9
MitchStark10 <mitch.stark.github@gmail.com>
2021-08-07 18:57:52 +0000

    mobile support for the nav menu

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,23 +7,26 @@ interface HeaderProps {
 
 export default function Header({ userEmail }: HeaderProps) {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" expand="md">
             <Container>
                 <Navbar.Brand href="/">Alarm Manager</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/features">Features</Nav.Link>
-                    <Nav.Link href="/pricing">Pricing</Nav.Link>
-                </Nav>
-                {userEmail ? (
-                    <Navbar.Text>
-                        Signed in as: <a href="/login">{userEmail}</a>
-                    </Navbar.Text>
-                ) : (
-                    <Nav className="user">
-                        <Button href="/sign-in">Sign In</Button>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/features">Features</Nav.Link>
+                        <Nav.Link href="/pricing">Pricing</Nav.Link>
                     </Nav>
-                )}
+                    {userEmail ? (
+                        <Navbar.Text>
+                            Signed in as: <a href="/login">{userEmail}</a>
+                        </Navbar.Text>
+                    ) : (
+                        <Nav className="user">
+                            <Button href="/sign-in">Sign In</Button>
+                        </Nav>
+                    )}
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
