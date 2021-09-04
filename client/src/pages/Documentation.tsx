0d6f63be5ce4f5911c38c8d33570eadb50f9e893
Mitch Stark <MitchStark10@gmail.com>
2021-09-04 14:16:54 -0400

    begin writing documentation

---
+++ client/src/pages/Documentation.tsx
@@ -0,0 +1,15 @@
+export default function Usage() {
+    return (
+        <>
+            <h1>Documentation</h1>
+            <p>
+                In order to use the API, a pre-built NodeJS module has been
+                built
+                <a href="https://github.com/MitchStark10/alarm-manager-node-client">
+                    here
+                </a>
+                .
+            </p>
+        </>
+    );
+}
--- client/src/pages/Installation.tsx
+++ client/src/pages/Installation.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Installation() {
-    return <h1>Installation Instructions Coming Soon</h1>;
+    return <h1>Installation Instructions</h1>;
 }
