2973a6640e0cfaa098ff3d18c2c01c126e927851
MitchStark10 <mitch.stark.github@gmail.com>
2021-09-05 11:41:44 -0400

    basic additional docs

--- client/src/pages/Documentation.tsx
+++ client/src/pages/Documentation.tsx
@@ -2,14 +2,18 @@ export default function Usage() {
     return (
         <>
             <h1>Documentation</h1>
+            <h2>Pre-Built Clients</h2>
             <p>
                 In order to use the API, a pre-built NodeJS module has been
                 built{' '}
                 <a href="https://github.com/MitchStark10/alarm-manager-node-client">
                     here
                 </a>
-                .
+                . More pre-built clients may come soon, but if you see a new for specific language support,
+                feel free to submit a PR in the <a href="https://github.com/MitchStark10/alarm-manager/issues">GitHub repo</a>.
             </p>
+            <h2>API Documentation</h2>
+            <p>To view the API documentation, go <a href="https://localhost:8080/api-docs">here</a>.</p>
 
             <h1>Services</h1>
         </>
