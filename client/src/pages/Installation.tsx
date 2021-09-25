71c70dcb6e9978b642a2242aa86bdf698851f860
MitchStark10 <mitch.stark.github@gmail.com>
2021-09-25 15:37:04 -0400

    Add note for another missing bit of instructions

--- client/src/pages/Installation.tsx
+++ client/src/pages/Installation.tsx
@@ -5,6 +5,7 @@ export default function Installation() {
         <ol>
             <li>To begin utilizing the project locally, clone the repo.</li>
             <li>Once cloned, run `npm install` at the base directory of the project.</li>
+            <li>TODO: Setup MySQL instructions</li>
             <li>TODO: Env variables setup</li>
             <li>You will be able to run the project in developer mode by using `npm run dev`. Otherwise, you can build the project for production mode using `npm run build`.</li>
             <li>Installation to an AWS Elastic Beanstalk instance has been setup through the release script and .ebignore file.</li>
