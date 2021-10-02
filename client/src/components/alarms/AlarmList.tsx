cca7955e3b5804de4ba976cb069d215e92a53f64
Mitch Stark <MitchStark10@gmail.com>
2021-10-02 19:08:01 -0400

    began work on aggregating alarms per title

--- client/src/components/alarms/AlarmList.tsx
+++ client/src/components/alarms/AlarmList.tsx
@@ -8,6 +8,10 @@ interface AlarmListProps {
     email?: string | null;
 }
 
+interface AlarmListGrouped {
+    [key: string]: AlarmData[];
+}
+
 export default function AlarmList({ email }: AlarmListProps) {
     const [alarmList, setAlarmList] = useState<Array<AlarmData>>([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -51,11 +55,27 @@ export default function AlarmList({ email }: AlarmListProps) {
         return <p>Loading...</p>;
     }
 
+    const alarmListGroupedByTitle = alarmList.reduce(
+        (aggregatedData: AlarmListGrouped, alarm: AlarmData): AlarmListGrouped => {
+            if (aggregatedData[alarm.AlarmTitle]) {
+                aggregatedData[alarm.AlarmTitle].push(alarm);
+            } else {
+                aggregatedData[alarm.AlarmTitle] = [alarm];
+            }
+
+            return aggregatedData;
+        },
+        {},
+    );
+
     return (
         <>
             <div className="d-flex flex-column justify-content-center align-items-center">
                 <h3 className="border-bottom border-dark px-5 mb-3">Alarms found</h3>
                 {error && <p className="error-text">{error.toString()}</p>}
+                {Object.entries(alarmListGroupedByTitle).map((keyValuePair, index) => (
+                    <p key={index}>{keyValuePair.toString()}</p>
+                ))}
                 {alarmList.length ? (
                     alarmList.map((alarmData) => (
                         <AlarmCard refreshAlarmList={forceAlarmListRefresh} data={alarmData} key={alarmData.ID} />
